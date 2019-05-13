d9167475294ee62b3b7de7da1c1f2606d17eaeb2
Eva Fann <exfann@gmail.com>
2019-05-13 15:49:43 -0400

    edited splice

--- problems/11-splice-element/11-splice-element.js
+++ problems/11-splice-element/11-splice-element.js
@@ -4,11 +4,16 @@ Splice the element at the provided index in the array.
 */
 function spliceElement(someArr, index) {
     let front = someArr.slice(0, index)
+    let elem = someArr[index]
     let back = someArr.slice(index+1, someArr.length)
     let spliced = front.concat(back)
-    return spliced
+    let ret = [[elem], spliced]
+    return ret
 }
 
 let arr = [1,2,3,4,5,6,7,8,9]
-
-console.log(spliceElement(arr, 5))
+let c = spliceElement(arr, 5)
+let elem2 = c[0]
+arr = c[1]
+console.log(elem2)
+console.log(arr)
